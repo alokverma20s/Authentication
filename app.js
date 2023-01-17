c8fa56bbfd75da72abc28dc93dd7555b7842fae4
Alok Verma <alokverma73762@gmail.com>
2023-01-17 21:50:49 +0530

    Using hashing for encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -23,7 +23,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -49,7 +48,7 @@ app.post("/register", function (req, res) {
             } else {
                 const newUser = new User({
                     email: req.body.username,
-                    password: req.body.password
+                    password: md5(req.body.password)
                 });
                 // console.log("Run");
                 newUser.save(function (err) {
@@ -66,7 +65,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: username}, function(err, foundUser){
         if(err){
             console.log(err);
